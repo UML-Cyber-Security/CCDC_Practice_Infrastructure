23000851b28ad0e06c4cc6b6a009b43158a7cf88
Matthew Harper <60448620+DaintyJet@users.noreply.github.com>
2024-02-10 14:44:04 -0500

    WIP

---
+++ Score-Check-Scripts/Linux/IPTables/Filter/OUTPUT-Default.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exit_code=0
+
+
+if(( $(iptables -L | grep -i "CHAIN OUTPUT (policy ACCEPT)" | wc -l ) != 0 )); then
+    echo "The INPUT Chain default policy is ACCEPT. For improved Security consider changing the default policy"
+    exit 1
+fi
+exit 0
+
