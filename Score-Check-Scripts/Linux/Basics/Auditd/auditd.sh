d5819b10735594642ecb43e039c78c4eb5e4551e
Matthew Harper <60448620+DaintyJet@users.noreply.github.com>
2024-02-10 16:10:48 -0500

    WIP Basics

---
+++ Score-Check-Scripts/Linux/Basics/Auditd/auditd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+exit_code=0
+
+
+if(( $(systemctl status auditd | grep -i "running" | wc -l ) == 0 )); then
+    echo "Auditd is not running, please consider running it to increase logging capabilities"
+    exit 1
+fi
+exit 0
+
