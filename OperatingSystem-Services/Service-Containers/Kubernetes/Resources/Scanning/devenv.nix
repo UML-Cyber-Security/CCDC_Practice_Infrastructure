2eba158642ac63733b3f2d5683707fa9efb7568d
SamD2021 <samuelramos852@gmail.com>
2025-01-19 23:04:32 -0500

    Add nix based k8s scanning tools
    This commit brings a md file holding some commands to install nix on any
    system, devenv to make isolated dev environments that comes with
    untempered tools, and direnv to load and unload those environments upon
    moving through directories.

---
+++ OperatingSystem-Services/Service-Containers/Kubernetes/Resources/Scanning/devenv.nix
@@ -0,0 +1,57 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  # This is where we add packages that we want in this directory, just save and exit to apply or use 'devenv shell'
+  packages = with pkgs; [
+    git
+    kubecm
+    kubeval
+    kubectl
+    kubescape
+    kube-hunter
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo hello from $GREET
+  '';
+  # Follow this format to add more scripts, the following script just lists all resources in the cluster
+  scripts.check-all.exec = ''
+    sudo kubectl get all -A
+  '';
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ OperatingSystem-Services/Service-Containers/Kubernetes/Resources/Scanning/nix-install.md
@@ -0,0 +1,21 @@
+# Installing nix
+
+```bash
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+  sh -s -- install
+```
+
+# Using devenv and direnv
+
+To install them to the system use:
+
+```bash
+nix profile install nixpkgs#direnv
+nix profile install nixpkgs#devenv
+```
+
+To manually activate the shell just run:
+
+```bash
+devenv shell
+```
